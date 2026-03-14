1d9f063dab44548f95460de662410744b29ac142
Rubeena-a <rubeenashaik889@gmail.com>
2026-03-14 20:26:56 +0530

    Updating Files

--- scripts/app-health.py
+++ scripts/app-health.py
@@ -1,12 +1,17 @@
+
 import requests
 
 url = "http://localhost:4499"
 
 try:
-    r = requests.get(url)
-    if r.status_code == 200:
+    response = requests.get(url)
+
+    if response.status_code == 200:
         print("Application is UP")
+        print("HTTP Status Code:", response.status_code)
     else:
         print("Application is DOWN")
-except:
+        print("HTTP Status Code:", response.status_code)
+
+except Exception as e:
     print("Application is not reachable")
--- scripts/system-health.sh
+++ scripts/system-health.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-CPU=$(top -bn1 | grep "Cpu" | awk '{print $2}')
-MEM=$(free | awk '/Mem/ {print $3/$2 * 100.0}')
+CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+MEM=$(free -m | awk 'NR==2{printf "%.2f", $3 * 100.0 / $2}')
 DISK=$(df / | awk 'END {print $5}')
 
 echo "CPU Usage: $CPU%"
 echo "Memory Usage: $MEM%"
 echo "Disk Usage: $DISK"
 
-if [ ${CPU%.*} -gt 80 ]; then
+CPU_INT=${CPU%.*}
+
+if [ "$CPU_INT" -gt 80 ]; then
 echo "ALERT: CPU usage is high!"
 fi
-
